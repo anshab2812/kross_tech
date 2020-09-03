38acdc4467bbbb37871a58a1fa21263ce680b572
anshab2812 <anshab2812gmail.com>
2020-09-03 15:11:51 +0200

    Labwork1

---
+++ labwork1/doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"labwork1","c":"Lab1","l":"Lab1()","url":"%3Cinit%3E()"},{"p":"labwork1","c":"Lab1","l":"main(String[])","url":"main(java.lang.String[])"}]
---
+++ labwork1/src/labwork1/Lab1.java
@@ -0,0 +1,30 @@
+package labwork1;
+/**
+ * 
+ * @author Stich
+ * @version 1.0
+ * @since 2020-09-02
+ *
+ */
+public class Lab1 {
+/**
+ * This method demonstrates short student information.
+ */
+	public static void main (String[] args) {
+		System.out.println("Group: 535st1");
+		System.out.println("Student: Shabal Anna Anatolievna");
+		System.out.println("Variant: 20");
+		String[] mas = {
+		" ****     ***       ***",
+		"**  **   ** **     ** **",
+		" **     **   **   **   **",
+		"  **   ********* *********",
+		"   **  **     ** **     **",
+		"**  ** **     ** **     **",
+		" ****  **     ** **     **"
+		};
+		for (String s:mas) {
+			System.out.println(s);
+		}
+	}
+}
